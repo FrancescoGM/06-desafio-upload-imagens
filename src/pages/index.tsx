390c70f9125026c6a63a22b35d4209e43ddf804b
Francesco <francescogmuller@hotmail.com>
2021-05-26 07:29:44 -0300

    feat: add first step in index page, next step is button to load next page data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,35 +8,66 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Card = {
+  id: string;
+  ts: number;
+  url: string;
+  title: string;
+  description: string;
+};
+
+type GetCardsResponse = {
+  data: Card[];
+  after: number | null;
+};
+
+type GetCardsProps = {
+  pageParam?: number | null;
+};
+
 export default function Home(): JSX.Element {
-  // const {
-  //   data,
-  //   isLoading,
-  //   isError,
-  //   isFetchingNextPage,
-  //   fetchNextPage,
-  //   hasNextPage,
-  // } = useInfiniteQuery(
-  //   'images',
-  //   // TODO AXIOS REQUEST WITH PARAM
-  //   ,
-  //   // TODO GET AND RETURN NEXT PAGE PARAM
-  // );
+  async function getCards({
+    pageParam = null,
+  }: GetCardsProps): Promise<GetCardsResponse> {
+    const { data } = await api.get<GetCardsResponse>('/api/images', {
+      params: { after: pageParam },
+    });
+    return data;
+  }
+  function getNextPageParam(cards: GetCardsResponse): number | null {
+    return cards.after || null;
+  }
+  const {
+    data,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<GetCardsResponse>('images', getCards, {
+    getNextPageParam,
+  });
 
-  // const formattedData = useMemo(() => {
-  //   // TODO FORMAT AND FLAT DATA ARRAY
-  // }, [data]);
+  const formattedData = useMemo(() => {
+    if (data) {
+      return data.pages[0].data || [];
+    }
+    return [];
+  }, [data]);
 
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        {/* <CardList cards={formattedData} /> */}
+        <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
